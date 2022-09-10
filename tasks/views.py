ea98842db68707a6dd6eaa34c5951e7d84101586
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-09-10 13:38:06 -0500

    feat(app): Initial commit

---
+++ tasks/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
