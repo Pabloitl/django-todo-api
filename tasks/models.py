76370e5000ddb3a255f4048c98a3be991f851fdc
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-09-12 08:28:12 -0500

    feat(tasks): Add delayed property

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+from datetime import date
+
 
 class Task(models.Model):
     title = models.CharField(max_length=100)
     due = models.DateField()
     description = models.TextField()
+
+    @property
+    def delayed(self) -> bool:
+        print(self.due, type(self.due))
+        return self.due < date.today()
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,9 +1,12 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from tasks.models import Task
 
 
 class TaskSerializer(ModelSerializer):
+    delayed = serializers.ReadOnlyField()
+
     class Meta:
         model = Task
         fields = "__all__"
