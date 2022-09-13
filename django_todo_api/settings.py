e5f963fe427c523fdb2aed54b4b681dcca359bf2
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-09-13 17:15:11 -0500

    feat(static): Change static files directory

--- django_todo_api/settings.py
+++ django_todo_api/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = Path(BASE_DIR).joinpath("staticfiles")
+STATIC_ROOT = Path(BASE_DIR).joinpath("static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
